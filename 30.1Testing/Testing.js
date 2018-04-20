030e2579aca283524d193c9bd273ef81cc30c331
letra <letra@bingyan.net>
2018-04-20 13:08:17 +0800

    update 30

--- 30.1Testing/Testing.js
+++ 30.1Testing/Testing.js
@@ -8,8 +8,9 @@ function foo() {
 // 30.2 No, but seriouly:
 /**
  * - Whichever testing framework you use, you should be writing tests!
- * - Strive to write many small pure functions, and minimize where mutations occur.
- * - Be cautious about stubs and jest at Airbnb. `tape` is also used occasionally for small, separate modules.
- * - 100% test coverage is a good goal to strive for, even if it's not always practical to reach it.
- * - Whether you fix a bug, write a regression test. A bug fixed without a regression test is almost certainly going to break again in the future.
+ * - Strive(力争) to write many small pure functions, and minimize where mutations occur.
+ * - Be cautious about stubs(存根) and mocks - they can make your test more brittle.
+ * - We primarily use `mocha` and `jest` at Airbnb. `tape` is also used occasionally for small, separate modules.
+ * - 100% test coverage(覆盖面) is a good goal to strive for, even if it's not always practical to reach it.
+ * - Whether you fix a bug, write a regression(倒退、回溯) test. A bug fixed without a regression test is almost certainly going to break again in the future.
  */
